bb66035dd1a679da2948ca05e12f575398593e4c
josefmvalek <jmvalek.business@gmail.com>
2024-03-14 17:52:09 +0100

    login and click on channel and click on members

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,17 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('test', async ({ page }) => {
+  await page.goto('https://web.telegram.org/a/');
+  await page.waitForTimeout(5000);
+  await page.getByRole('button', { name: 'Log in by phone Number' }).click();
+  await page.getByLabel('Your phone number').click();
+  await page.waitForTimeout(1000);
+  await page.getByLabel('Your phone number').fill('+595985600811');
+  await page.getByRole('button', { name: 'Next' }).click();
+  await page.waitForTimeout(10000);
+  await page.getByLabel('Code').fill('');
+  await page.getByRole('button', { name: 'LIFE | Жизнь в АРГЕНТИНЕ LIFE' }).click();
+  await page.locator('#MiddleColumn').getByRole('button', { name: 'LIFE | Жизнь в АРГЕНТИНЕ' }).click();
+  await page.getByRole('button', { name: 'Marina Yakovleva', exact: true }).click();
+  await page.pause()
 });
